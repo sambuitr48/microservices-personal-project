9d9b26dd9fed12385b73a98ca3d15914a9e2d9bd
sambuitr48 <buitragoosoriosamuel@gmail.com>
2024-11-21 21:49:37 -0500

    1.0 se dockeriza la aplicación-1.1

--- product-service/db/dbConfig.js
+++ product-service/db/dbConfig.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: '',
-database: 'product_db'
+host: process.env.DB_HOST || 'localhost',
+user: process.env.DB_USER || 'root',
+password: process.env.DB_PASSWORD || '',
+database: process.env.DB_NAME || 'product_db'
 });
 
 db.connect(err => {
